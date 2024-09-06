c7fc1e5d3f2ec0c28a240e4c67570d49716a7305
Krouta1 <116441828+Krouta1@users.noreply.github.com>
2024-09-06 19:26:11 +0200

    trying to fix webhook :)

--- app/Models/UserSchema.ts
+++ app/Models/UserSchema.ts
@@ -3,10 +3,12 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema(
   {
     clerkUserId: { type: String, unique: true, required: true },
-    emailAddress: { type: String, unique: true },
+    emailAddress: { type: String, unique: true, required: true }, // Ensure email is required
   },
   { timestamps: true },
 );
+
+// Handling model compilation in environments like Next.js
 const User = mongoose.models.User || mongoose.model("User", userSchema);
 
 export default User;
